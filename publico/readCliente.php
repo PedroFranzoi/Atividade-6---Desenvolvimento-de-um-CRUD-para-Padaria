d8259456d8aaf2f866db86a9e932a9ec3a2fe339
PedroFranzoi <pedro_franzoi@estudante.sesisenai.org.br>
2025-08-14 10:04:08 -0300

    Inicio da tabela de compras

---
+++ publico/readCliente.php
@@ -0,0 +1,43 @@
+<?php
+
+include '../privado/db.php';
+
+
+$sql = "SELECT * FROM produto";
+
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+
+    echo "<table border = '1'>
+        <tr>
+            <th> Nome </th>
+            <th> Preço </th>
+            <th> Descrição </th>
+            <th> Disponiveis </th>
+            <th> Criador </th>
+        </tr>
+    ";
+
+    while($row = $result->fetch_assoc()){
+
+        echo "<tr>
+                <td> {$row['nome']} </td>
+                <td> {$row['preco']} </td>
+                <td> {$row['descricao']} </td>
+                <td> {$row['quantidade_estoque']} </td>
+                <td> {$row['id_usuario']} </td>
+                <td>
+                    <a href=''>Comprar</a>
+                </td>
+            </tr>
+        ";
+    }
+    echo "</table>";
+}else{
+    echo "Nenhum produto registrado.";
+}
+
+$conn -> close();
+
+?>
