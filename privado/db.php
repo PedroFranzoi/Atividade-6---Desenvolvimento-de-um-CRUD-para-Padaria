0610ac566db9e2437c59053ce3134dae95402ffc
Mordecai <heitor_emmerich@estudante.sesisenai.org.br>
2025-08-14 08:53:50 -0300

    add - nova conexao banco de dados

--- privado/db.php
+++ privado/db.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "bumba_meu_pao";
+$dbname = "padaria";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
